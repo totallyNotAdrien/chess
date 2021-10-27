19ca86e264a76308d4c158c02fd040e5cf39ccdc
Tina Thompson <mark_junior13@yahoo.com>
2021-10-27 07:56:24 -0500

    Board planning

---
+++ lib/board.rb
@@ -0,0 +1,17 @@
+class Board
+  def initialize(moves = [])
+    #grid 8x8 array
+  end
+
+  def move_piece(start_pos, end_pos)
+    #check for piece at start_pos
+      #check that end_pos is valid for piece
+        #move piece
+        #side effects
+          #pawn that moved 2 spaces is made en passantable to now-adjacent pawns
+  end
+
+  def display
+    #
+  end
+end
