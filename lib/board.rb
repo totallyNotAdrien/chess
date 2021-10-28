c9aef56524a19556f4190fd9c765114a114e3b53
Tina Thompson <mark_junior13@yahoo.com>
2021-10-28 09:04:44 -0500

    Board#display in progress

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,9 @@
 class Board
   def initialize(moves = [])
     #grid 8x8 array
+    @rows = 8
+    @cols = 8
+    @grid = Array.new(@rows){Array.new(@cols)}
   end
 
   def move_piece(start_pos, end_pos)
@@ -12,6 +15,95 @@ class Board
   end
 
   def display
-    #
+    puts
+
+    output = ""
+    #build output row by row
+    for row_index in 0...@rows
+      row_number = @rows-row_index
+      row_out = " #{row_number}" + (" " * 1)
+
+      bg_color = row_index % 2
+      piece = "\u265e"
+
+      for col_index in 0...@cols
+        #sym = colored_piece(@grid[row_index][col_index])
+        grid_space = color_space(" #{piece} ".black, bg_color)
+        last_col = col_index == @cols - 1
+        row_out += " #{grid_space}#{last_col ? " " : ""}"
+        bg_color = (bg_color + 1) % 2
+      end
+      output += "#{row_out}\n"
+      #output += (" " * 3) + "#{grid_row_separator}\n" unless row_index == @rows - 1
+    end
+
+    puts output
+
+    #column indicators
+    col_num_output = " " * 5
+    for letter in "a".."h"
+      col_num_output += "#{letter}" + (" " * 4)
+    end
+    puts col_num_output
+  end
+
+  def color_space(contents, color_index)
+    return contents.on_brown if color_index == 0
+    contents.on_blue
+  end
+
+  def colored_piece(thing)
+    "m"
+  end
+
+  def em
+    "\u2014"
+  end
+
+  def grid_row_separator
+    thing = "+ #{em * 2} "
+    " #{em * 2} #{thing * (@cols - 1)}"
+  end
+end
+
+class String
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def black
+    colorize(30)
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def yellow
+    colorize(33)
+  end
+
+  def on_red
+    colorize(41)
+  end
+
+  def on_brown
+    colorize(43)
+  end
+
+  def on_blue
+    colorize(44)
+  end
+
+  def on_cyan
+    colorize(46)
+  end
+
+  def on_peach
+    colorize(101)
+  end
+
+  def on_yellow
+    colorize(103)
   end
 end
---
+++ lib/main.rb
@@ -0,0 +1,4 @@
+require_relative "chess.rb"
+
+board = Board.new
+board.display
