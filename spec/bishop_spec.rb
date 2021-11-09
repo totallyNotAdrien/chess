034e3b3bc85f1f137b115d540a7661bfbb9336a3
Tina Thompson <mark_junior13@yahoo.com>
2021-11-09 08:57:04 -0600

    add more tests for Bishop#moves

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -30,8 +30,48 @@ describe Bishop do
         #@board.display      #uncomment to show setup
       end
 
-      it "can go to g2 and h3" do
+      it "can go to edge of board" do
         expect(@piece.moves).to contain_exactly("g2", "h3")
+      end
+    end
+
+    context "when a direction is blocked by enemy piece" do
+
+      before(:each) do
+        @board = newly_set_up_board
+        @grid = @board.grid
+        pieces = @grid[0] + @grid[1] + @grid[6] + @grid[7]
+        pieces.each {|piece| allow(piece).to receive(:valid_move?).and_return(true)}
+
+        @board.move_piece("g2","g3")
+        @board.move_piece("f7","f5")
+        @board.move_piece("f1","h3")
+        @piece = @grid[5][7]
+        #@board.display      #uncomment to show setup
+      end
+
+      it "can move to, but not beyond, enemy piece position" do
+        expect(@piece.moves).to contain_exactly("f1", "g2", "g4", "f5")
+      end
+    end
+
+    context "when a direction is blocked by ally piece" do
+
+      before(:each) do
+        @board = newly_set_up_board
+        @grid = @board.grid
+        pieces = @grid[0] + @grid[1] + @grid[6] + @grid[7]
+        pieces.each {|piece| allow(piece).to receive(:valid_move?).and_return(true)}
+
+        @board.move_piece("d2","d5")
+        @board.move_piece("g2","g3")
+        @board.move_piece("f1","g2")
+        @piece = @grid[6][6]
+        #@board.display      #uncomment to show setup
+      end
+
+      it "can move no further than space closest to ally, between ally and piece" do
+        expect(@piece.moves).to contain_exactly("f1", "f3", "e4", "h3")
       end
     end
   end
