e4d32999081243ef5196d021aa427e8527452221
Tina Thompson <mark_junior13@yahoo.com>
2021-11-19 14:43:25 -0600

    add remaining castling requirements to King#moves

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -34,28 +34,38 @@ class King < Piece
       piece_at_pos && (piece_at_pos.color == color || piece_at_pos.is_a?(GhostPawn))
     end
 
-
     #castling stuff
-    #goes here
     row_index, col_index = chess_to_grid_coordinates(@position) || @position
 
+    #kingside_castle
     kingside_rook = @board.grid[row_index][7]
-    kingside_others = @board.grid[row_index][(col_index + 1)..(col_index+2)]
+    kingside_others = @board.grid[row_index][(col_index + 1)..(col_index + 2)]
+    kingside_positions = (0..2).map { |offset| [row_index, col_index + offset] }
 
-    queenside_rook = @board.grid[row_index][0]
-    queenside_others = @board.grid[row_index][(col_index - 3)..(col_index-1)]
+    kingside_under_attack = kingside_positions.any? do |pos|
+      @board.under_attack_from_color?((@color + 1) % 2, pos)
+    end
 
-    if !moved && kingside_others.all?{|piece| piece == nil} && 
-      kingside_rook && !kingside_rook.moved
+    if !moved && kingside_others.all? { |piece| piece == nil } &&
+       kingside_rook && !kingside_rook.moved && !kingside_under_attack
       out.push([row_index, col_index + 2])
     end
 
-    if !moved && queenside_others.all?{|piece| piece == nil} && 
-      queenside_rook && !queenside_rook.moved
+    #queenside castle
+    queenside_rook = @board.grid[row_index][0]
+    queenside_others = @board.grid[row_index][(col_index - 3)..(col_index - 1)]
+    queenside_positions = (-2..0).map { |offset| [row_index, col_index + offset] }
+
+    queenside_under_attack = queenside_positions.any? do |pos|
+      @board.under_attack_from_color?((@color + 1) % 2, pos)
+    end
+
+    if !moved && queenside_others.all? { |piece| piece == nil } &&
+       queenside_rook && !queenside_rook.moved && !queenside_under_attack
       out.push([row_index, col_index - 2])
     end
 
-    out.map!{|coords| grid_to_chess_coordinates(coords)}
+    out.map! { |coords| grid_to_chess_coordinates(coords) }
     out
   end
 
@@ -66,20 +76,20 @@ class King < Piece
     new_row_index, new_col_index = chess_to_grid_coordinates(pos) || pos
 
     #castling
-    if !moved && new_row_index == row_index && 
-      (new_col_index - col_index).abs == 2
+    if !moved && new_row_index == row_index &&
+       (new_col_index - col_index).abs == 2
       rook_col = nil
       new_rook_col = nil
 
       rook = if new_col_index > col_index
-        rook_col = 7
-        new_rook_col = col_index + 1
-        @board.grid[row_index][rook_col]
-      else
-        rook_col = 0
-        new_rook_col = col_index - 1
-        @board.grid[row_index][rook_col]
-      end
+          rook_col = 7
+          new_rook_col = col_index + 1
+          @board.grid[row_index][rook_col]
+        else
+          rook_col = 0
+          new_rook_col = col_index - 1
+          @board.grid[row_index][rook_col]
+        end
 
       if rook
         @board.grid[row_index][rook_col] = nil
@@ -87,7 +97,7 @@ class King < Piece
         rook.set_pos([row_index, new_rook_col])
       end
     end
-      
+
     super(pos)
   end
 end
